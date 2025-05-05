47438b7589bfe00672e29e6bbc7b6f8bd910ed18
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-05-05 16:22:44 -0400

    Compilador

---
+++ ProjetoLexPascal/LexPascal/src/Mecanismo/Executor.java
@@ -0,0 +1,206 @@
+package Mecanismo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Executor {
+
+    private final String captureComment = "\\/\\/.*|\\(\\*(?:.|\\s)*\\*\\)";
+    private final String captureNumbers = "(?<!\\w)(?:-?\\d+(?:\\.\\d+)?)(?!\\w)";
+    private final String captureLiteral = "'(?:[^']|'')*'";
+    private final String captureWords   = "\\w+";
+    private final String captureCharacters = "(?::=|>=|<=|<>|>|<|=|\\+|\\-|\\*|\\/|[:;,.()\\[\\]{}])";
+    private final String captureIdentifier = "^[A-Za-z][A-Za-z0-9_]*$";
+    private String capture;
+
+    private BufferedReader reader;
+    private ArrayList<String> bufferPrimario;
+    private ArrayList<String> bufferSecundario;
+
+    @SuppressWarnings("unused")
+    private HashMap<String, Token> tabelaSimbolosPrograma;
+
+    @SuppressWarnings("unused")
+    private boolean IsNumber(String valor)
+    {
+        Pattern pattern = Pattern.compile(this.captureNumbers);
+        Matcher matcher = pattern.matcher(valor);
+        if (matcher.find() == true){
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressWarnings("unused")
+    private boolean IsLiteral(String valor)
+    {
+        Pattern pattern = Pattern.compile(this.captureLiteral);
+        Matcher matcher = pattern.matcher(valor);
+        if (matcher.find() == true){
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressWarnings("unused")
+    private boolean IsCharacter(String valor)
+    {
+        Pattern pattern = Pattern.compile(this.captureCharacters);
+        Matcher matcher = pattern.matcher(valor);
+        if (matcher.find() == true){
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressWarnings("unused")
+    private boolean IsIdentifier(String valor)
+    {
+        Pattern pattern = Pattern.compile(this.captureIdentifier);
+        Matcher matcher = pattern.matcher(valor);
+        if (matcher.find() == true){
+            return true;
+        }
+        return false;
+    }
+
+    public void CarregarArquivo(){
+        System.out.println("----------------------------------------");
+        System.out.println("##### Carregar Arquivo Pascal #####");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o diretório do arquivo: ");
+        String diretorio = scanner.next();
+        System.out.print("Digite o nome do arquivo (com extensão .pas): ");
+        String nomeArquivo = scanner.next();
+        String caminhoCompleto = diretorio + "/" + nomeArquivo;
+        this.CarregarBufferPrimario(caminhoCompleto);
+        scanner.close();        
+    }
+
+    public void CarregarArquivo(String caminhoCompleto){
+        System.out.println("----------------------------------------");
+        System.out.println("##### Carregar Arquivo Pascal #####");
+        this.CarregarBufferPrimario(caminhoCompleto);
+    }
+
+    private void CarregarBufferPrimario(String caminhoCompleto){
+        this.reader = null;
+        try {
+            this.reader = new BufferedReader(new FileReader(caminhoCompleto));
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
+
+    public void ProcessarBufferPrimario(){
+        this.bufferPrimario = new ArrayList<>();
+        try {
+            String linha;
+            while ((linha = this.reader.readLine()) != null) {
+                bufferPrimario.add(linha);
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Erro ao fechar o arquivo: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void ImprimirBufferPrimario(){
+        System.out.println("----------------------------------------");
+        System.out.println("##### Conteúdo do Buffer primário: #####");
+        for (String texto : this.bufferPrimario) {
+            System.out.println(texto);
+        }
+        System.out.println("----------------------------------------");        
+    }
+
+    public void ProcessarBufferSecundario()
+    {        
+        this.capture = captureComment.concat("|")
+            .concat(captureNumbers).concat("|")
+            .concat(captureLiteral).concat("|")
+            .concat(captureWords).concat("|")
+            .concat(captureCharacters);
+
+        this.bufferSecundario = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile(this.capture);
+
+        for (String texto : bufferPrimario) {
+            Matcher matcher = pattern.matcher(texto);
+            while(matcher.find()){
+                String lexema = matcher.group();
+                if (this.bufferSecundario.contains(lexema) == false){
+                    this.bufferSecundario.add(lexema);
+                }
+            }
+        }
+        bufferSecundario.removeIf(value -> value.startsWith("//") || value.startsWith("(*"));
+    } 
+    
+    public void ImprimirBufferSecundario(){
+        System.out.println("----------------------------------------");
+        System.out.println("##### Conteúdo do Buffer secundário: #####");
+        for (String texto : this.bufferSecundario) {
+            System.out.println(texto);
+        }
+        System.out.println("----------------------------------------");        
+    }
+
+    public void AnalisarMontandoTabelaSimbolos(){
+        this.tabelaSimbolosPrograma = new HashMap<>();
+
+        for (String lexema : this.bufferSecundario) {
+            Token token = new Token();
+            
+            if (IsNumber(lexema)) {
+                token.setTipo("NUMERO");
+            } else if (IsLiteral(lexema)) {
+                token.setTipo("LITERAL");
+            } else if (IsCharacter(lexema)) {
+                token.setTipo("CARACTERE");
+            } else if (IsIdentifier(lexema)) {
+                token.setTipo("IDENTIFICADOR");
+            } else {
+                token.setTipo("DESCONHECIDO");
+            }
+        
+            token.setLexema(lexema);
+            this.tabelaSimbolosPrograma.put(lexema, token);
+        }
+
+      
+        for (String lexema : this.tabelaSimbolosPrograma.keySet()) {
+            Token token = this.tabelaSimbolosPrograma.get(lexema);
+            System.out.println("Lexema: " + token.getLexema() + " | Tipo: " + token.getTipo());
+        }
+
+        //AQUI É QUE ENTRA O TRABALHO DE VCS.
+        //1 - Precisa da Tabela de Simbolos do programa.
+        //2 - Precisa da Tabela de Simbolos da linguagem.
+        //3 - Precisa varrer o buffer secundário, para localizar os tokens, definindo o que é cada um dos lexemas.
+    }
+
+    public void ImprimirTabelaSimbolosPrograma(){
+      System.out.println("----------------------------------------");
+    System.out.println("##### Tabela de Símbolos do Programa #####");
+    for (Token token : this.tabelaSimbolosPrograma.values()) {
+        System.out.println(token);
+    }
+    System.out.println("----------------------------------------");
+    }
+
+}
